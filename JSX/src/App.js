33cc2b49422e8b97b73c1a1b02309f47aa58ea03
Irfan Rasheed <125676445+Irfan-Rasheed-186@users.noreply.github.com>
2023-02-23 21:54:59 +0500

    Add files via upload

---
+++ JSX/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name="Irfan";
+function App() {
+  return (
+    <>
+    <div className="blank"><h1>Welcome to the React JSX</h1></div>
+    <h2>Hello {name}</h2>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed assumenda tempore eum. Deserunt eveniet facere velit iure temporibus, nisi, Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero repellat velit non quisquam harum iure earum dolorem ab repudiandae, porro nihil accusantium similique alias nostrum ut aliquam ullam consequatur distinctio? sint ut similique odio quis autem obcaecati ipsam hic ipsa. Quaerat.</div>
+    </>
+  );
+}
+
+export default App;
