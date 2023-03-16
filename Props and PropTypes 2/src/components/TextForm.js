37790aaaed150da94213fe505ff60c8ffb7802b8
Irfan Rasheed <125676445+Irfan-Rasheed-186@users.noreply.github.com>
2023-03-16 22:57:24 +0500

    Add files via upload

---
+++ Props and PropTypes 2/src/components/TextForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function TextForm(props) {
+  return (
+    <div>
+    <h2>{props.heading}</h2>
+      <div className="mb-3">
+        <textarea className="form-control" id="myBox" rows="8"></textarea>
+      </div>
+    </div>
+  );
+}
